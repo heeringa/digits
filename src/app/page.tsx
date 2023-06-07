602c52288971652bc289bf46c32cee2a3f9321ec
Brent Heeringa <heeringa@mindburner.io>
2023-06-07 14:00:55 -0400

    Updated to include typescript type linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import styles from './styles.module.css';
+//import styles from './styles.module.css';
 
 
 export default function Page() {
