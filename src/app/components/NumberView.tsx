7e3d56c67132990df9ee1bab2f9ad6d205939887
Brent Heeringa <brentheeringa@gmail.com>
2023-06-23 12:45:22 -0400

    cursor default keeps the pointer ans select-none makes the text unselectable (#10)
    Co-authored-by: Brent Heeringa <heeringa@mindburner.io>

--- src/app/components/NumberView.tsx
+++ src/app/components/NumberView.tsx
@@ -16,17 +16,17 @@ interface NumberProps {
 function NumberView({ index, value, isEditable=true, isSelected=false, isVisible=true, onNumberClick, onChange }:NumberProps): React.JSX.Element {
 
   const basestyle = "flex items-center justify-center h-24 w-24 rounded-3xl border border-gray-800";
-  let style = isEditable ? `${basestyle} ${styles.pulse}` : basestyle;
+  let style = isEditable ? `${basestyle} ${styles.pulse}` : `${basestyle}`;
   style = isSelected ? `${style} bg-blue-500 text-white` : `${style} bg-white text-black`;
   style = isVisible ? `${style}` : `${style} opacity-0 pointer-events-none`;
+  
 
   // focus:ring-2 focus:ring-gray-200
   return (
     <div className={style}
          id={`pos-${index}`}
          onClick={event => onNumberClick(index, event)}>
-     <input className="bg-transparent text-center text-3xl w-20 focus:border-gray-300 
-                       focus:outline-none" 
+     <input className={"bg-transparent text-center text-3xl w-20 focus:border-gray-300 focus:outline-none" + (isEditable ? "" : " cursor-default select-none")} 
             type="number"
             placeholder = ""
             readOnly={!isEditable} 
