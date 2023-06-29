f451301fb80d505280dac890ceebd21f0c97e3cb
Brent Heeringa <brentheeringa@gmail.com>
2023-06-29 12:46:20 -0400

    remove lint errors

--- api/tests/test_digits.py
+++ api/tests/test_digits.py
@@ -1,6 +1,7 @@
 import pytest
 from digits import combine, generate_solutions
 
+
 def test_combine():
     assert combine('+', 1, 2) == 3
     assert combine('-', 5, 2) == 3
@@ -8,6 +9,7 @@ def test_combine():
     assert combine('/', 4, 2) == 2
     with pytest.raises(ZeroDivisionError):
         combine('/', 4, 0)
+
 
 def test_generate_solutions():
     DIGITS = [1, 2, 3]
@@ -21,6 +23,7 @@ def test_generate_solutions():
     sols = generate_solutions(DIGITS, OPS, GOAL)
 
     assert sols == expected_result
+
 
 def test_generate_solutions_no_solution():
     DIGITS = [1, 2, 3]
