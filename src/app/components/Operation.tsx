f5aa910277839842aa6948f7a15ff6b9458d3057
Brent Heeringa <brentheeringa@gmail.com>
2023-07-02 09:38:57 -0400

    7 make arithmetic glyphs look nice (#19)
    * Sub in Flowbite-react Button component and flowbite react icons
    Update the logic accordingly
    
    * back to purple
    
    * remove event in onNumberClick to help linter

--- src/app/components/Operation.tsx
+++ src/app/components/Operation.tsx
@@ -1,28 +1,33 @@
+'use client';
+
 import React from 'react';
+import { Button } from 'flowbite-react';
+import { IconType } from 'react-icons';
+
 
 interface OperationProps {
-  isEditable: boolean;
+  disabled: boolean;
   isSelected: boolean;
-  value: string;
-  onOperationClick: (value: string, event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  Icon: IconType;
+  op: string;
+  onOperationClick: (value: string, event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
-function Operation({ isEditable=true, isSelected=false, value, onOperationClick }:OperationProps): React.JSX.Element {
+function Operation({ disabled=true, isSelected=false, Icon, op, onOperationClick }:OperationProps): React.JSX.Element {
 
-  const basestyle = "flex items-center justify-center h-12 w-12 rounded-full border border-gray-800";
-  let style = isEditable ? `${basestyle}` : basestyle; 
-  style = isSelected ? `${style} bg-blue-800 text-white` : `${style} bg-white text-black`
-  // focus:ring-2 focus:ring-gray-200
-  
+
+  const basestyle = "flex items-center justify-center";
+  const color = isSelected ? "success" : "purple";
+
   return (
-    <div className={style}
-         onClick={event => onOperationClick(value, event)}>
-     <input className="text-center text-2xl w-8 focus:border-gray-300 bg-transparent 
-                       focus:outline-none" 
-            readOnly={true}
-            value={value}
-            />
+    <div className={basestyle}>
+      <Button disabled={disabled} 
+              color={color}
+              onClick={event => onOperationClick(op, event)}>
+        <Icon />
+      </Button>
     </div>
+
   );
 }
 
